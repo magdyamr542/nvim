99fc7c0a9d97d3081988ae537d21b3177fcd6e77
Amr Metwally <amr.metwally@lynq.tech>
2023-03-05 18:57:40 +0200

    Added comments to plugins

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -22,63 +22,60 @@ vim.cmd([[
   augroup end
 ]])
 
-
-
 return require('packer').startup(function(use)
-        -- Packer can manage itself
-        use 'wbthomason/packer.nvim'
-        use 'Mofiqul/vscode.nvim'
-        use 'jiangmiao/auto-pairs'
-        use 'tomasiser/vim-code-dark'
-        use 'ayu-theme/ayu-vim'
-        use 'ambv/black'
-        use 'nvim-lua/popup.nvim'
-        use 'nvim-lua/plenary.nvim'
-        use 'nvim-telescope/telescope.nvim'
-        use 'sheerun/vim-polyglot'
-        use 'scrooloose/NERDTree'
-        use 'RRethy/vim-illuminate'
-        use 'sbdchd/neoformat'
-        use 'pangloss/vim-javascript'
-        use 'leafgarland/typescript-vim'
-        use 'morhetz/gruvbox'
-        use 'unblevable/quick-scope'
-        use 'liuchengxu/vim-which-key'
-        use 'tpope/vim-surround'
-        use 'preservim/nerdcommenter'
-        use { 'iamcco/markdown-preview.nvim', run = 'cd app && yarn install' }
-        use 'EdenEast/nightfox.nvim'
-        use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
-        use 'nvim-treesitter/playground'
-        use 'nvim-treesitter/nvim-treesitter-context'
-        use 'neovim/nvim-lspconfig'
-        use 'hrsh7th/cmp-nvim-lsp'
-        use 'hrsh7th/cmp-buffer'
-        use 'hrsh7th/cmp-path'
-        use 'hrsh7th/cmp-cmdline'
-        use 'hrsh7th/nvim-cmp'
-        use 'hrsh7th/cmp-vsnip'
-        use 'hrsh7th/vim-vsnip'
-        use 'hrsh7th/vim-vsnip-integ'
-        use 'hrsh7th/cmp-nvim-lsp-signature-help'
-        use 'ryanoasis/vim-devicons'
-        use { 'akinsho/bufferline.nvim', tag = 'v3.*' }
-        use 'nvim-tree/nvim-web-devicons'
-        use 'rafamadriz/friendly-snippets'
-        use 'yioneko/nvim-yati'
-        use { 'dstein64/nvim-scrollview', branch = 'main' }
-        use 'karb94/neoscroll.nvim'
-        use { 'mg979/vim-visual-multi', branch = 'master' }
-        use { 'rose-pine/neovim', branch = 'main' }
-        use 'lukas-reineke/indent-blankline.nvim'
-        use 'nvim-lualine/lualine.nvim'
-        use 'lewis6991/gitsigns.nvim'
-        use 'j-hui/fidget.nvim'
-        use 'numToStr/Comment.nvim'
-        use 'williamboman/mason.nvim'
-        use 'williamboman/mason-lspconfig.nvim'
-        use 'navarasu/onedark.nvim'
-        use 'norcalli/nvim-colorizer.lua'
+        use 'wbthomason/packer.nvim'                                           -- Packer itself
+        use 'Mofiqul/vscode.nvim'                                              -- Vscode like color schemes
+        use 'jiangmiao/auto-pairs'                                             -- Automatically insert matching brackets, parentheses, and quotes
+        use 'tomasiser/vim-code-dark'                                          -- Dark color scheme
+        use 'ayu-theme/ayu-vim'                                                -- Color scheme
+        use 'ambv/black'                                                       -- Python formatter
+        use 'nvim-lua/popup.nvim'                                              -- Popup windows
+        use 'nvim-lua/plenary.nvim'                                            -- Utils library for Lua plugins
+        use 'nvim-telescope/telescope.nvim'                                    -- Fuzzy finder
+        use 'sheerun/vim-polyglot'                                             -- Language pack for syntax highlighting
+        use 'scrooloose/NERDTree'                                              -- File system explorer
+        use 'RRethy/vim-illuminate'                                            -- Highlight other occurrences of the word under the cursor
+        use 'sbdchd/neoformat'                                                 -- Code formatter
+        use 'pangloss/vim-javascript'                                          -- JavaScript syntax highlighting
+        use 'leafgarland/typescript-vim'                                       -- TypeScript syntax highlighting
+        use 'morhetz/gruvbox'                                                  -- Color scheme
+        use 'unblevable/quick-scope'                                           -- Highlight characters used for motions
+        use 'liuchengxu/vim-which-key'                                         -- Show key mappings in a popup
+        use 'tpope/vim-surround'                                               -- Easy manipulation of surroundings
+        use 'preservim/nerdcommenter'                                          -- Commenting shortcuts
+        use { 'iamcco/markdown-preview.nvim', run = 'cd app && yarn install' } -- Preview Markdown files in browser
+        use 'EdenEast/nightfox.nvim'                                           -- Color scheme
+        use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }           -- Syntax highlighting and parsing
+        use 'nvim-treesitter/playground'                                       -- Treesitter playground for experimenting with treesitter queries
+        use 'nvim-treesitter/nvim-treesitter-context'                          -- Show context of cursor location
+        use 'neovim/nvim-lspconfig'                                            -- Configuration for language servers
+        use 'hrsh7th/cmp-nvim-lsp'                                             -- Autocomplete plugin
+        use 'hrsh7th/cmp-buffer'                                               -- Autocomplete source for buffer words
+        use 'hrsh7th/cmp-path'                                                 -- Autocomplete source for filesystem paths
+        use 'hrsh7th/cmp-cmdline'                                              -- Autocomplete source for command line history
+        use 'hrsh7th/nvim-cmp'                                                 -- Autocomplete plugin
+        use 'hrsh7th/cmp-vsnip'                                                -- Autocomplete source for snippets
+        use 'hrsh7th/vim-vsnip'                                                -- Snippet plugin
+        use 'hrsh7th/vim-vsnip-integ'                                          -- Integration between nvim-cmp and vim-vsnip
+        use 'hrsh7th/cmp-nvim-lsp-signature-help'                              -- Show signature help for autocomplete
+        use 'ryanoasis/vim-devicons'                                           -- Icons for file types and plugins
+        use { 'akinsho/bufferline.nvim', tag = 'v3.*' }                        -- Display buffer tabs with configurable colors and icons.
+        use 'nvim-tree/nvim-web-devicons'                                      -- A Lua library for file icon support in Neovim.
+        use 'rafamadriz/friendly-snippets'                                     -- A collection of snippets for various programming languages.
+        use 'yioneko/nvim-yati'                                                -- A simple and fast indent line plugin for Neovim.
+        use { 'dstein64/nvim-scrollview', branch = 'main' }                    -- Displays a preview of the file contents during scrolling.
+        use 'karb94/neoscroll.nvim'                                            -- A smooth scrolling plugin for Neovim.
+        use { 'mg979/vim-visual-multi', branch = 'master' }                    -- A multiple cursors plugin for Neovim.
+        use { 'rose-pine/neovim', branch = 'main' }                            -- A color scheme for Neovim.
+        use 'lukas-reineke/indent-blankline.nvim'                              -- A plugin to display indent lines in Neovim.
+        use 'nvim-lualine/lualine.nvim'                                        -- A fast and customizable status line plugin for Neovim.
+        use 'lewis6991/gitsigns.nvim'                                          -- A Git gutter plugin for Neovim.
+        use 'j-hui/fidget.nvim'                                                -- A lightweight file explorer for Neovim.
+        use 'numToStr/Comment.nvim'                                            -- A plugin to easily comment and uncomment code in various filetypes.
+        use 'williamboman/mason.nvim'                                          -- Portable package manager for Neovim
+        use 'williamboman/mason-lspconfig.nvim'                                -- A LSP configuration for the Mason build language.
+        use 'navarasu/onedark.nvim'                                            -- A dark color scheme for Neovim.
+        use 'norcalli/nvim-colorizer.lua'                                      -- A plugin to highlight color codes in various filetypes.
 
         -- Automatically set up your configuration after cloning packer.nvim
         -- Put this at the end after all plugins
