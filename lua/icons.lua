7347987a0614b959b6ef881916fa244d786a5718
amr <magdyamr542@gmail.com>
2024-09-15 14:54:09 +0200

    icons

---
+++ lua/icons.lua
@@ -0,0 +1,16 @@
+return {
+  diagnostics = {
+    BoldError = "",
+    Error = "",
+    BoldWarning = "",
+    Warning = "",
+    BoldInformation = "",
+    Information = "",
+    BoldQuestion = "",
+    Question = "",
+    BoldHint = "",
+    Hint = "󰌶",
+    Debug = "",
+    Trace = "✎",
+  },
+}
