27a364bf487a0de7afc7f5971721879e328c4ef4
amr <magdyamr542@gmail.com>
2024-11-02 13:29:18 +0100

    enable c and rust

--- lua/treesitter.lua
+++ lua/treesitter.lua
@@ -1,16 +1,10 @@
 require 'nvim-treesitter.configs'.setup {
   -- A list of parser names, or "all"
-  ensure_installed = { "javascript", "python", "typescript", "json", "html", "go", "vim", "lua" },
+  ensure_installed = { "javascript", "python", "typescript", "json", "html", "go", "vim", "lua", "c", "rust" },
 
   highlight = {
     -- `false` will disable the whole extension
     enable = true,
-
-    -- NOTE: these are the names of the parsers and not the filetype. (for example if you want to
-    -- disable highlighting for the `tex` filetype, you need to include `latex` in this list as this is
-    -- the name of the parser)
-    -- list of language that will be disabled
-    disable = { "c", "rust" },
 
     -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
     -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
